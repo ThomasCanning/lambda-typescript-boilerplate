71ea5286c05c98573978bada1d965e2ec3b4f696
thomascanning <tom@thomascanning.co.uk>
2025-11-17 17:58:05 +0000

    method level error types

--- src/lib/jmap/errors.ts
+++ src/lib/jmap/errors.ts
@@ -2,24 +2,48 @@
 
 import { StatusCodes } from "http-status-codes"
 
-export const unknownCapabilityUrn: string = "urn:ietf:params:jmap:error:unknownCapability" as const
+export const unknownCapabilityUrn = "urn:ietf:params:jmap:error:unknownCapability" as const
 
-export const notJsonUrn: string = "urn:ietf:params:jmap:error:notJSON" as const
+export const notJsonUrn = "urn:ietf:params:jmap:error:notJSON" as const
 
-export const notRequestUrn: string = "urn:ietf:params:jmap:error:notRequest" as const
+export const notRequestUrn = "urn:ietf:params:jmap:error:notRequest" as const
 
 //Must include limit property (name of limit) in problem details response
-export const limitUrn: string = "urn:ietf:params:jmap:error:limit" as const
+export const limitUrn = "urn:ietf:params:jmap:error:limit" as const
 
 export type RequestErrorType =
-  | typeof unknownCapabilityUrn
-  | typeof notJsonUrn
-  | typeof notRequestUrn
-  | typeof limitUrn
+  | "urn:ietf:params:jmap:error:unknownCapability"
+  | "urn:ietf:params:jmap:error:notJSON"
+  | "urn:ietf:params:jmap:error:notRequest"
+  | "urn:ietf:params:jmap:error:limit"
 
-export type requestError = {
+export type RequestError = {
   type: RequestErrorType
   status: StatusCodes.BAD_REQUEST
   detail: string
   limit?: string
+}
+
+// Method level errors
+
+export type MethodErrorType =
+  | "serverUnavailable"
+  | "serverFail"
+  | "serverPartialFail"
+  | "unknownMethod"
+  | "invalidArguments"
+  | "invalidResultReference"
+  | "forbidden"
+  | "accountNotFound"
+  | "accountNotSupportedByMethod"
+  | "accountReadOnly"
+  | string
+
+export type MethodError = {
+  methodName: "error"
+  arguments: {
+    type: MethodErrorType
+    description?: string
+  }
+  methodCallId: string
 }
