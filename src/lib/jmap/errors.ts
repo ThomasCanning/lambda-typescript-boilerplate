4ba2dd74f7a376ee88cca2ab982f4ac3db4b705a
thomascanning <tom@thomascanning.co.uk>
2025-11-17 17:40:08 +0000

    request level types

--- src/lib/jmap/errors.ts
+++ src/lib/jmap/errors.ts
@@ -1,8 +1,25 @@
-export type unknownCapabilityUrn = "urn:ietf:params:jmap:error:unknownCapability"
+// Request level error urns
 
-export type notJsonUrn = "urn:ietf:params:jmap:error:notJSON"
+import { StatusCodes } from "http-status-codes"
 
-export type notRequestUrn = "urn:ietf:params:jmap:error:notRequest"
+export const unknownCapabilityUrn: string = "urn:ietf:params:jmap:error:unknownCapability" as const
+
+export const notJsonUrn: string = "urn:ietf:params:jmap:error:notJSON" as const
+
+export const notRequestUrn: string = "urn:ietf:params:jmap:error:notRequest" as const
 
 //Must include limit property (name of limit) in problem details response
-export type limitUrn = "urn:ietf:params:jmap:error:limit"
+export const limitUrn: string = "urn:ietf:params:jmap:error:limit" as const
+
+export type RequestErrorType =
+  | typeof unknownCapabilityUrn
+  | typeof notJsonUrn
+  | typeof notRequestUrn
+  | typeof limitUrn
+
+export type requestError = {
+  type: RequestErrorType
+  status: StatusCodes.BAD_REQUEST
+  detail: string
+  limit?: string
+}
