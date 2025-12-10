f096afa85691da75177eac9d909a4d89eb50a10e
thomascanning <tom@thomascanning.co.uk>
2025-12-10 18:24:00 +0000

    refactor workflow

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -8,40 +8,40 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from "./routes/__root"
-import { Route as IndexRouteImport } from "./routes/index"
-import { Route as GenerateJobIdRouteImport } from "./routes/generate.$jobId"
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as GenerateJobIdRouteImport } from './routes/generate.$jobId'
 
 const IndexRoute = IndexRouteImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
 const GenerateJobIdRoute = GenerateJobIdRouteImport.update({
-  id: "/generate/$jobId",
-  path: "/generate/$jobId",
+  id: '/generate/$jobId',
+  path: '/generate/$jobId',
   getParentRoute: () => rootRouteImport,
 } as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute
-  "/generate/$jobId": typeof GenerateJobIdRoute
+  '/': typeof IndexRoute
+  '/generate/$jobId': typeof GenerateJobIdRoute
 }
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute
-  "/generate/$jobId": typeof GenerateJobIdRoute
+  '/': typeof IndexRoute
+  '/generate/$jobId': typeof GenerateJobIdRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
-  "/": typeof IndexRoute
-  "/generate/$jobId": typeof GenerateJobIdRoute
+  '/': typeof IndexRoute
+  '/generate/$jobId': typeof GenerateJobIdRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: "/" | "/generate/$jobId"
+  fullPaths: '/' | '/generate/$jobId'
   fileRoutesByTo: FileRoutesByTo
-  to: "/" | "/generate/$jobId"
-  id: "__root__" | "/" | "/generate/$jobId"
+  to: '/' | '/generate/$jobId'
+  id: '__root__' | '/' | '/generate/$jobId'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
@@ -49,19 +49,19 @@ export interface RootRouteChildren {
   GenerateJobIdRoute: typeof GenerateJobIdRoute
 }
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/": {
-      id: "/"
-      path: "/"
-      fullPath: "/"
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
       preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
-    "/generate/$jobId": {
-      id: "/generate/$jobId"
-      path: "/generate/$jobId"
-      fullPath: "/generate/$jobId"
+    '/generate/$jobId': {
+      id: '/generate/$jobId'
+      path: '/generate/$jobId'
+      fullPath: '/generate/$jobId'
       preLoaderRoute: typeof GenerateJobIdRouteImport
       parentRoute: typeof rootRouteImport
     }
